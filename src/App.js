171422ecb77ecf4813db56ff7d10929cc12937fb
adelhighrullin <adelhighrullin@gmail.com>
2021-08-06 14:13:39 +0300

    works with proper name of the city

--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from "./Header";
 import WeatherBlock from "./WeatherBlock";
-//import Footer from "./Footer";
-
-
-
-//const getWeather = async () => {
-
-//}
 
 function App() {
 
   const API_KEY = "c889405c4f6d4e738b175950212907";
-  let q = "London";
-  let aqi = "no";
-  let temp_c = 0;
 
-  const [state, setstate] = useState({ temp: "" });
+  //gotta make a list of modes (current, forecast, etc) to choose one that is needed
+  let mode = "current";
+  let aqi = "no";
+
+  const [info, setInfo] = useState({
+    city: "",
+    country: "",
+    temp_c: "",
+    cond: "",
+    icon: ""
+  });
+
+  const [query, setQuery] = useState("London");
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getWeather();
+  }, [query]);
 
   const getWeather = async () => {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${q}&aqi=${aqi}`);
+
+    /*
+    setQuery({
+      query: ""
+    });
+    */
+
+    const response = await fetch(`https://api.weatherapi.com/v1/${mode}.json?key=${API_KEY}&q=${query}&aqi=${aqi}`);
     const data = await response.json();
     console.log(data);
     //const parse_data = await JSON.parse(data);
-    temp_c = await data["current"]["temp_c"];
-    console.log(temp_c);
 
-    setstate({ temp: temp_c });
+    //i should change it to key -> value, because right now it seems done bad
+    const city = await data["location"]["name"];
+    const country = await data["location"]["country"];
+    const temp_c = await data["current"]["temp_c"];
+    const cond = await data["current"]["condition"]["text"];
+    const icon = await data["current"]["condition"]["icon"];
+
+    setInfo({
+      city: city,
+      country: country,
+      temp_c: temp_c,
+      cond: cond,
+      icon: icon
+    });
+
   }
+
+  const updateSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
+  const sendQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
     <div className="App">
       <Header />
-      <form className="Search">
-        <input type="text" placeholder="Type your city here: " />
-        <button type="button" onClick={getWeather}>Search</button>
+      <form className="Search" onSubmit={sendQuery}>
+        <input type="text" placeholder="Type your city here: " value={search} onChange={updateSearch}/>
+        <button type="submit" >Search</button>
       </form>
       <WeatherBlock
-        temp_c={state.temp}
+        city={info.city}
+        country={info.country}
+        temp_c={info.temp_c}
+        cond={info.cond}
+        icon={info.icon}
       />
     </div>
   );
--- src/WeatherBlock.js
+++ src/WeatherBlock.js
@@ -3,7 +3,10 @@ import React from 'react';
 function WeatherBlock(props) {
   return (
     <div className="WeatherBlock">
-      <p>Температура: {props.temp_c}</p>
+      <p>{props.city}, {props.country}</p>
+      <p>Temperature: {props.temp_c}&deg;C</p>
+      <p>{props.cond}</p>
+      <img src={props.icon} alt={props.condition}/>
     </div>
   );
 }
