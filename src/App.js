471772586985c274cf16ad9943afd7b0ea77b110
adelhighrullin <adelhighrullin@gmail.com>
2021-08-09 15:35:36 +0300

    it doesn't shut server down with big error when typo

--- src/App.js
+++ src/App.js
@@ -23,24 +23,34 @@ function App() {
 
   const [search, setSearch] = useState("");
 
+  const [responseCode, setResponseCode] = useState(true);
+
   useEffect(() => {
-    getWeather();
+    getResponse();
   }, [query]);
 
-  const getWeather = async () => {
-
-    /*
-    setQuery({
-      query: ""
-    });
-    */
-
+  const getResponse = async () => {
+    //don't know whether it's correct or not, but i works as i want it to work (:D)
     const response = await fetch(`https://api.weatherapi.com/v1/${mode}.json?key=${API_KEY}&q=${query}&aqi=${aqi}`);
+    setResponseCode(response.ok);
+    if (response.ok) {
+      getWeather(response);
+    }
+  }
+
+  const getWeather = async (response) => {
+
+
+    //TODO: make exceptions via try..catch to give an answer when the city is not found
+    //const response = await fetch(`https://api.weatherapi.com/v1/${mode}.json?key=${API_KEY}&q=${query}&aqi=${aqi}`);
+    //setResponseCode(response.status);
+
     const data = await response.json();
     console.log(data);
     //const parse_data = await JSON.parse(data);
 
     //i should change it to key -> value, because right now it seems done bad
+    //or maybe make it as a list or smth
     const city = await data["location"]["name"];
     const country = await data["location"]["country"];
     const temp_c = await data["current"]["temp_c"];
@@ -72,16 +82,23 @@ function App() {
     <div className="App">
       <Header />
       <form className="Search" onSubmit={sendQuery}>
-        <input type="text" placeholder="Type your city here: " value={search} onChange={updateSearch}/>
+        <input type="text" placeholder="Type your city here: " value={search} onChange={updateSearch} />
         <button type="submit" >Search</button>
       </form>
-      <WeatherBlock
-        city={info.city}
-        country={info.country}
-        temp_c={info.temp_c}
-        cond={info.cond}
-        icon={info.icon}
-      />
+      <div>
+        {
+          responseCode
+            ? <WeatherBlock
+              city={info.city}
+              country={info.country}
+              temp_c={info.temp_c}
+              cond={info.cond}
+              icon={info.icon}
+            />
+            : <p>no matches found. please check if the city was spelled correctly</p>
+        }
+      </div>
+
     </div>
   );
 }
